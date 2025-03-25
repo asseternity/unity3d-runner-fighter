f3cea279cb9d4d8657ab71099d336f53b3d653d5
asseternity <nakupovasset@gmail.com>
2025-03-25 23:48:00 +0500

    feat: init turn based battle system

--- Assets/Scripts/BattlePlayerController.cs
+++ Assets/Scripts/BattlePlayerController.cs
@@ -1,4 +1,6 @@
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BattlePlayerController : MonoBehaviour
 {
@@ -11,6 +13,23 @@ public class BattlePlayerController : MonoBehaviour
     [Tooltip("The current grid position (x,y) where the player currently is")]
     private Vector3Int currentGridPosition;
 
+    // ui
+    public GameObject turnData;
+    public GameObject movementData;
+    public GameObject endTurnButton;
+
+    // turn data trackers
+    private bool playersTurn = true;
+    private int playerMovementLeft = 6;
+
+    // to do here:
+    // [v] 1) create a battle system ui:
+    // that shows whose turn it is
+    // that lets you end your turn
+    // [v] 2) make movement only possible when it's your turn
+    // [v] 3) make movement a limited resource that drains as you move and resets on next turn
+    // [v] 4) add movement as a bar to ui
+
     void Start()
     {
         // Convert the grid coordinate to a world position.
@@ -22,49 +41,81 @@ public class BattlePlayerController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        if (playersTurn && playerMovementLeft > 0)
         {
-            Vector3Int desiredGridPosition = new Vector3Int(
-                currentGridPosition.x - 1,
-                currentGridPosition.y,
-                currentGridPosition.z
-            );
-            Vector3 worldPos = grid.CellToWorld(desiredGridPosition) + grid.cellSize / 2f;
-            transform.position = worldPos;
-            currentGridPosition = desiredGridPosition;
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                Vector3Int desiredGridPosition = new Vector3Int(
+                    currentGridPosition.x - 1,
+                    currentGridPosition.y,
+                    currentGridPosition.z
+                );
+                Vector3 worldPos = grid.CellToWorld(desiredGridPosition) + grid.cellSize / 2f;
+                transform.position = worldPos;
+                currentGridPosition = desiredGridPosition;
+                playerMovementLeft--;
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                Vector3Int desiredGridPosition = new Vector3Int(
+                    currentGridPosition.x + 1,
+                    currentGridPosition.y,
+                    currentGridPosition.z
+                );
+                Vector3 worldPos = grid.CellToWorld(desiredGridPosition) + grid.cellSize / 2f;
+                transform.position = worldPos;
+                currentGridPosition = desiredGridPosition;
+                playerMovementLeft--;
+            }
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                Vector3Int desiredGridPosition = new Vector3Int(
+                    currentGridPosition.x,
+                    currentGridPosition.y + 1,
+                    currentGridPosition.z
+                );
+                Vector3 worldPos = grid.CellToWorld(desiredGridPosition) + grid.cellSize / 2f;
+                transform.position = worldPos;
+                currentGridPosition = desiredGridPosition;
+                playerMovementLeft--;
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                Vector3Int desiredGridPosition = new Vector3Int(
+                    currentGridPosition.x,
+                    currentGridPosition.y - 1,
+                    currentGridPosition.z
+                );
+                Vector3 worldPos = grid.CellToWorld(desiredGridPosition) + grid.cellSize / 2f;
+                transform.position = worldPos;
+                currentGridPosition = desiredGridPosition;
+                playerMovementLeft--;
+            }
         }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            Vector3Int desiredGridPosition = new Vector3Int(
-                currentGridPosition.x + 1,
-                currentGridPosition.y,
-                currentGridPosition.z
-            );
-            Vector3 worldPos = grid.CellToWorld(desiredGridPosition) + grid.cellSize / 2f;
-            transform.position = worldPos;
-            currentGridPosition = desiredGridPosition;
-        }
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            Vector3Int desiredGridPosition = new Vector3Int(
-                currentGridPosition.x,
-                currentGridPosition.y + 1,
-                currentGridPosition.z
-            );
-            Vector3 worldPos = grid.CellToWorld(desiredGridPosition) + grid.cellSize / 2f;
-            transform.position = worldPos;
-            currentGridPosition = desiredGridPosition;
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Vector3Int desiredGridPosition = new Vector3Int(
-                currentGridPosition.x,
-                currentGridPosition.y - 1,
-                currentGridPosition.z
-            );
-            Vector3 worldPos = grid.CellToWorld(desiredGridPosition) + grid.cellSize / 2f;
-            transform.position = worldPos;
-            currentGridPosition = desiredGridPosition;
-        }
+        // now update ui
+        Text movementDataText = movementData.GetComponent<Text>();
+        movementDataText.text = playerMovementLeft.ToString();
+    }
+
+    public void EndTurn()
+    {
+        playersTurn = false;
+
+        // update ui
+        Text turnDataText = turnData.GetComponent<Text>();
+        turnDataText.text = "enemy";
+
+        EnemyTurn();
+    }
+
+    async void EnemyTurn()
+    {
+        await Task.Delay(2000);
+        playersTurn = true;
+        playerMovementLeft = 6;
+
+        // update ui
+        Text turnDataText = turnData.GetComponent<Text>();
+        turnDataText.text = "player";
     }
 }
